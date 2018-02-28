1a1d87768f2b4feb4799079424322fb57ee4fe2f
bhuvi12 <31438639+bhuvi12@users.noreply.github.com>
2018-02-28 15:39:54 +0530

    Create 50power.C

---
+++ 50power.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("\n enter number");
+scanf("%d",&n);
+if(n/2)
+{
+printf("\n given no is power of 2\n");
+}
+else
+{
+printf("\n not a power of 2");
+}
+return 0;
+}
