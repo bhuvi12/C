b107314774aa88822216e0001578ac8e7a8dbd29
bhuvi12 <31438639+bhuvi12@users.noreply.github.com>
2018-02-28 15:47:03 +0530

    Create 82area.C

---
+++ 82area.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+double l,b,a;
+printf("\n enter the breadh");
+scanf("%lf",&b);
+printf("\n enter the length");
+scanf("%lf",&l);
+a=b*l;
+printf("\n%lf",a);
+return 0;
+}
